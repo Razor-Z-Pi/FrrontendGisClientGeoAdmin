55c041a588bd09885c5be066847f7e4e6d5b8f3e
Razor_Z_Pi <popovp.popoff2001@gmail.com>
2022-08-02 11:22:56 +0700

    new file developer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginForm/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginForm from './components/Auth/Login/Login.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginForm
   }
 }
 </script>
---
+++ src/components/Auth/Login/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <section>
+    <h1 class="Title">Welcome User</h1>
+
+    <div class="Tower">
+      <div class="left-box-tower">
+
+      </div>
+      <div class="right-box-tower">
+
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Login"
+}
+</script>
+
+<style scoped>
+html {
+  box-sizing: border-box;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
+img {
+  width: 100%;
+}
+
+a {
+  text-decoration: none;
+}
+
+.Title {
+  text-align: center;
+  font-size: 40px;
+  margin: 0;
+  padding: 0;
+}
+</style>
