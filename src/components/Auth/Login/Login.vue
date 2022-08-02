854a374ca032ad2eb4f76071961c67d0e9d0d945
Razor_Z_Pi <popovp.popoff2001@gmail.com>
2022-08-02 18:21:09 +0700

    auth new

--- src/components/Auth/Login/Login.vue
+++ src/components/Auth/Login/Login.vue
@@ -1,50 +1,132 @@
 <template>
-  <section>
-    <h1 class="Title">Welcome User</h1>
+  <section class="container">
+    <h1 class="Title">Авторизация</h1>
 
     <div class="Tower">
-      <div class="left-box-tower">
+        <div class="con-form">
+          <vs-input v-model="email" placeholder="Почта">
+            <template #icon>
+              @
+            </template>
+          </vs-input>
+          <vs-input type="password" v-model="password" placeholder="Пароль">
+            <template #icon>
+              <img src="../../../assets/lock.png" width="25" />
+            </template>
+          </vs-input>
+          <div class="flex">
+            <vs-checkbox success v-model="option2">
+              <template #icon>
+                <i class='bx bx-check-double'></i>
+              </template>
+              Запомнить
+            </vs-checkbox>
+            <a href="#">Забыли пароль?</a>
+          </div>
+          <div class="footer-dialog">
+            <vs-button block>
+              Войти
+            </vs-button>
 
+            <div class="new">
+              Нет аккаунта? <a href="#">Создайть аккаунт!</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="right-box-tower">
-
-      </div>
-    </div>
   </section>
 </template>
 
 <script>
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: "Login"
+  name: "Login",
+  data: () => ({
+    active: false,
+    email: '',
+    password: '',
+    remember: false,
+    option2: false,
+  })
 }
 </script>
 
-<style scoped>
-html {
-  box-sizing: border-box;
-}
+<style lang="stylus">
 
-*, *:before, *:after {
-  box-sizing: border-box;
-}
+.Title
+  text-align center
+  font-size 40px
+  margin 0
+  padding 0
 
-body {
-  margin: 0;
-}
 
-img {
-  width: 100%;
-}
+.Tower
+  display flex
+  justify-content center
+  align-items center
 
-a {
-  text-decoration: none;
-}
+  width 100%
+  height 100%
 
-.Title {
-  text-align: center;
-  font-size: 40px;
-  margin: 0;
-  padding: 0;
-}
+
+/*Vuesax Auth*/
+
+getColor(vsColor, alpha = 1)
+  unquote("rgba(var(--vs-" + vsColor + "), " +alpha + ")")
+
+getVar(var)
+  unquote("var(--vs-" + var + ")")
+
+.not-margin
+  margin 0px
+  font-weight normal
+  padding 10px
+
+.con-form
+  width 500px
+
+  .flex
+    display flex
+    align-items center
+    justify-content space-between
+
+    a
+      font-size .8rem
+      opacity .7
+
+      &:hover
+        opacity 1
+
+  .vs-checkbox-label
+    font-size .8rem
+
+  .vs-input-content
+    margin 10px 0px
+    width calc(100%)
+
+    .vs-input
+      width 100%
+
+.footer-dialog
+  display flex
+  align-items center
+  justify-content center
+  flex-direction column
+  width calc(100%)
+
+  .new
+    margin 0px
+    margin-top 20px
+    padding: 0px
+    font-size .7rem
+
+    a
+      color getColor('primary') !important
+      margin-left 6px
+
+      &:hover
+        text-decoration underline
+
+  .vs-button
+    margin 0px
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+
+Vue.use(Vuesax)
 
 Vue.config.productionTip = false
 
